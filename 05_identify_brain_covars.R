2cdb7f5560157e3a0cc6351b7aa6e19ba0cd165e
alorchhota <alorchhota@gmail.com>
2017-10-04 12:09:54 -0400

    made MARS work

--- 05_identify_brain_covars.R
+++ 05_identify_brain_covars.R
@@ -1,6 +1,18 @@
 library('earth')
-#library('parallel')
+library('parallel')
 library('argparser')
+library('svd')
+
+numerical_covariates = c("AGE", "BMI", "HGHT", "WGHT", "DTHRFGD", "DTHVNTD", 
+                         "MH_PC1", "MH_PC2", "MH_PC3", "TRCCLMPD", "TRCHSTIND", 
+                         "TRCRTMP", "TRISCHD", "TRDNISCH", 
+                         "SMRIN", "SMTSISCH", "SMTSPAX")
+categorical_covariates = c("COHORT", "ETHNCTY", "SEX", "INCEXC", "RACE", 
+                           "DTHATPSY", "DTHCODD_CAT", "DTHHRDY", 
+                           "DTHRFG", "DTHVNT", "DTHCLS", "DTHTYP", "DTHCAT", "DTHICD10",
+                           "TRAMP", "TRORGNS", 
+                           "SMNABTCH", "SMGEBTCH", "SMCAT", "SMCENTER", 
+                           "SMOMTRLTP", "SMSTYP", "SMTSD", "ANALYTE_TYPE", "SMTORMVE")
 
 getArgs <- function() {
   parser <- arg_parser(description='EarthSelect for covariates')
@@ -13,7 +25,7 @@ getArgs <- function() {
   parser <- add_argument(parser, '-regress', help='Regress this covariate out of the others prior to selection; comma for multiple', type='character', default=NULL)
   parser <- add_argument(parser, '-jackknife', help='Downsample to this many samples', type='integer', default=0)
   parser <- add_argument(parser, '-within', help='Run within groups (one per replicate)', type='character', default="")
-
+  parser <- add_argument(parser, '-min_sample', help='In a categorical variable, a category must have a min number of samples, otherwise set to UNKNOWN', default=15)
   arrv <- parse_args(parser)
 }
 
@@ -51,15 +63,24 @@ earthSelect <- function(gene.expr, covars, n.predictors=100, num.genes=50, min.s
   } else {
     gene.idx <- 1:nrow(gene.expr)
   }
+  
+  ### compute expression PCs
+  expr_mat_transposed = scale(t(gene.expr[gene.idx,]))   # sample x gene
+  expr_svd = propack.svd(expr_mat_transposed, neig = min(dim(expr_mat_transposed)))
+  pcs = t(expr_svd$u[,1:20])
+  colnames(pcs) = rownames(expr_mat_transposed)
+  rownames(pcs) = paste0('pc', 1:nrow(pcs))
+  var_exp_by_pc = (expr_svd$d^2)/sum((expr_svd$d^2))
+  
+  
   print('before lhs')
-  lhs <- paste('cbind(', paste(rownames(gene.expr)[gene.idx], collapse=', '), ')', sep='')
+  lhs <- paste('cbind(', paste(rownames(pcs), collapse=', '), ')', sep='')
   print('after lhs')
   rhs1 <- paste(colnames(covars), collapse=' + ')
-  num.covs <- sapply(1:ncol(covars), function(c.idx) {
-    ! is.factor(covars[,c.idx]) && ! is.character(covars[,c.idx])
-  })
-  num.covs <- colnames(covars)[num.covs]
+  num.covs <- intersect(colnames(covars), numerical_covariates)
   covars[,num.covs] <- scale(covars[,num.covs])  # put covariates to mean=0, var=1
+  
+  
   print('before force.linear')
   if ( force.linear ) {
     rhs2 <- paste(sapply(num.covs, function(u) { paste('I(', u, '^2)', sep='')}), collapse=' + ')
@@ -78,8 +99,9 @@ earthSelect <- function(gene.expr, covars, n.predictors=100, num.genes=50, min.s
   } else {
     lins <- FALSE
   }
-  my.data <- cbind(covars, scale(t(gene.expr[gene.idx,])))  # set genes to mean-0, var-1
-  print(paste0('#NA in my.data', sum(is.na(my.data))))
+  
+  my.data <- cbind(covars, t(pcs))
+  print(paste0('#NA in my.data: ', sum(is.na(my.data))))
   n.terms <- NULL
   print('before earth call')
   eres <- earth(fla, data=my.data, trace=2, degree=3, nk=n.predictors, pmethod='backward', 
@@ -91,8 +113,11 @@ earthSelect <- function(gene.expr, covars, n.predictors=100, num.genes=50, min.s
   }
   while ( is.null(n.terms) || n.terms > max.terms ) {
     eres <- update(eres, nprune=nprune)
-    coefs <- as.data.frame(sort(apply(eres$coefficients, 1, function(x) { quantile(abs(x), 0.8)}), decreasing=T))
-    colnames(coefs) <- 'Beta80Pct'
+    multiplied_coefs <- eres$coefficients
+    for(npc in 1:ncol(multiplied_coefs))
+      multiplied_coefs[,npc] = multiplied_coefs[,npc] * var_exp_by_pc[npc] * 100 
+    coefs <- as.data.frame(sort(apply(multiplied_coefs, 1, function(x) { max(abs(x))}), decreasing=T))
+    colnames(coefs) <- 'BetaMax'
     if ( 'mean.expr' %in% rownames(coefs) ) {
       drop.idx <- which(rownames(coefs) == 'mean.expr')
       coefs <- coefs[1:(drop.idx-1),,drop=F]
@@ -105,7 +130,8 @@ earthSelect <- function(gene.expr, covars, n.predictors=100, num.genes=50, min.s
         terms[[term.idx]] <- c(terms[[term.idx]], colnames(covars)[c.idx])
       }
     }
-    terms <- lapply(terms, function(s) { paste(s, collapse=':')})
+    #terms <- lapply(terms, function(s) { paste(s, collapse=':')})
+    terms <- lapply(terms, function(s) { if (is.null(s)) return(""); return(paste(sort(s), collapse=':'))})
     terms <- unique(unlist(terms))
     if ( '' %in% terms ) {
       terms[terms == ''] <- '1'
@@ -114,6 +140,7 @@ earthSelect <- function(gene.expr, covars, n.predictors=100, num.genes=50, min.s
     mm <- model.matrix(formula(new.fla), covars)
     n.terms <- ncol(mm)
     nprune = nprune - 1
+    print(paste('n.terms:', n.terms, ', nprune:', nprune))
   }
   list('earth'=eres, 'formula'=new.fla, 'terms'=terms, 'model.matrix'=mm, 'term.imp'=coefs)
 }
@@ -121,39 +148,27 @@ earthSelect <- function(gene.expr, covars, n.predictors=100, num.genes=50, min.s
 multiEarthSelect <- function(gene.expr, covars, n.replicates=10, n.cores=4, n.jk=0, w.grp=NULL) {
   # run earthSelect `n.replicates` times on 1,000 random samples of genes
   # and return the model importances
-  #to.ret <- mclapply(1:n.replicates, function(r) {
-  to.ret <- lapply(1:n.replicates, function(r) {
+  to.ret <- mclapply(1:n.replicates, function(r) {
     this.expr <- gene.expr
     this.cov <- covars
-    if (  ! is.null(w.grp) && w.grp != "" ) {
-      grps <- unique(this.cov[,w.grp])
-      grp.idx <- which(this.cov[,w.grp] == sample(grps, 1))
-      if ( length(grp.idx) < 20 ) {  # pick an additional group if too small
-        grp.idx <- unique(c(grp.idx, which(this.cov[,w.grp] %in% sample(grps, 2))))
-      }
-      this.expr <- this.expr[,grp.idx]
-      this.cov <- this.cov[grp.idx,]
-      print(dim(this.cov))
-      # drop unique factors
-      drop.cov <- apply(this.cov, 2, function(x) length(unique(as.character(x))) == 1)
-      if ( length(which(drop.cov)) > 0 ) {
-        this.cov <- this.cov[,-which(drop.cov)]
-      }
-      print(dim(this.cov))
-    }
     if ( n.jk > 0 ) {
       # jackknife
       cat(sprintf('Jackknifing %d samples\n', n.jk))
       s.idx <- sample.int(ncol(this.expr), n.jk)
       this.expr <- this.expr[,s.idx]
       this.cov <- this.cov[s.idx,]
-      res <- earthSelect(this.expr, this.cov, n.predictors=80, num.genes=1000, nprune=50, max.terms=50)
+      res <- earthSelect(this.expr, this.cov, n.predictors=80, num.genes=1e6, nprune=80, max.terms=150)
     } else {
       print('before earchSelect call')
-      print(paste0('#NA in expr: ', sum(is.na(this.expr))))
-      n_na = sapply(this.cov, function(x) sum(is.na(x)))
-      print(n_na)
-      res <-  earthSelect(this.expr, this.cov, n.predictors=80, num.genes=1000, nprune=50, max.terms=50)
+      
+      # # todo: handle covariates correctly so that there is no NA
+      # print(paste0('#NA in expr: ', sum(is.na(this.expr))))
+      # n_na = sapply(this.cov, function(x) sum(is.na(x)))    
+      # this.cov = this.cov[,n_na==0]
+      # n_uniq = sapply(this.cov, function(x) length(unique(x)))
+      # this.cov = this.cov[,n_uniq>1]
+      
+      res <-  earthSelect(this.expr, this.cov, n.predictors=100, num.genes=1e6, nprune=80, max.terms=150)
       print('after earchSelect call')
     }
     print(names(res))
@@ -162,60 +177,63 @@ multiEarthSelect <- function(gene.expr, covars, n.replicates=10, n.cores=4, n.jk
     } else {
       NULL
     }
-  #}, mc.cores=n.cores)   # parallel
-  })
-  save(to.ret, file='foo.Rda')
+  }, mc.cores=n.cores)   # parallel
+  save(to.ret, file='foo.Rda')   # todo: save it properly
   to.ret <- to.ret[which(sapply(to.ret, function(x) ! is.null(x)))]
   to.ret
 }
 
-main <- function(xp.file, cov.file, out.file, do.log, n.rep, n.cores, regress.covar, jackknife, group.within) {
-  dat.expr <- read.table(xp.file, header=T, row.names=1)
+main <- function(xp.file, cov.file, out.file, do.log, n.rep, n.cores, regress.covar, jackknife, group.within, min.samples) {
+  #xp.file=args$expression; cov.file = args$covariates; out.file = args$output; do.log = args$log; n.rep=args$replicates; n.cores=args$ncores; regress.covar=args$regress; jackknife=args$jackknife; group.within=args$within; min.samples=args$min_sample;
+  dat.expr <- read.table(xp.file, header=T, row.names=1, sep='\t')
   if ( do.log > 0 ) {
     dat.expr <- log2(1e-3 + dat.expr)
   }
   colnames(dat.expr) <- make.names(colnames(dat.expr))
-  dat.cov <- read.table(cov.file, header=T, row.names=1, stringsAsFactors=F)
-  rownames(dat.cov) <- make.names(rownames(dat.cov))
-  dat.cov$ETHNCTY = factor(dat.cov$ETHNCTY)
-  dat.cov$SEX = factor(dat.cov$SEX)
-  dat.cov$RACE = factor(dat.cov$RACE)
   
-  dat.cov = dat.cov[, !colnames(dat.cov) %in% c("SMMNCV","SM550NRM")]  # exclude column with NA
-  n_uniq = sapply(dat.cov, function(x) length(unique(x)))
-  dat.cov = dat.cov[, n_uniq > 1]
+  dat.cov <- read.table(cov.file, header=T, row.names=1, stringsAsFactors=F, sep='\t', colClasses = "character")  # todo: exclude SM* and MH* covariates
+  rownames(dat.cov) <- make.names(dat.cov$st_id)
   
   i.samples <- intersect(rownames(dat.cov), colnames(dat.expr))
-  dat.cov <- dat.cov[i.samples,]
-  # hack -- regress out st_id from the other covariates
-  if ( 'st_id' %in% colnames(dat.cov) ) {
-    dat.cov <- dat.cov[,-which(colnames(dat.cov) == 'st_id')]
-  }
-  if ( 'SUBJID' %in% colnames(dat.cov) ) {
-    dat.cov <- dat.cov[,-which(colnames(dat.cov) == 'SUBJID')]
-  }
-  if ( ! is.null(regress.covar) ) {
-    if ( grepl(',', regress.covar) ) {
-      regress.covars <- unlist(strsplit(regress.covar, ','))
-    } else {
-      regress.covars <- c(regress.covar)
-    }
-    for ( regress.covar in regress.covars ) {
-      cat(sprintf('Regressing %s from other covariates....\n\n', regress.covar))
-      for ( col.i in 1:ncol(dat.cov) ) {
-        if ( colnames(dat.cov)[col.i] == regress.covar ) {
-          next
-        }
-        if ( is.numeric(dat.cov[,col.i]) ) { 
-          ddf <- data.frame(y=dat.cov[,col.i], x=dat.cov[,regress.covar])
-          lm.res <- lm(y ~ x, data=ddf)
-          dat.cov[,col.i] <- lm.res$residuals
-        }
-      }
-    }
-  }
   dat.expr <- dat.expr[,i.samples]
-  # res <- earthSelect(dat.expr, dat.cov)
+  dat.cov <- dat.cov[i.samples,]
+  
+  #### process covariate data
+  num_cov_df = dat.cov[,numerical_covariates]
+  cat_cov_df = dat.cov[,categorical_covariates]
+  
+  n_na_num = sapply(num_cov_df, function(x) sum(is.na(x)))
+  num_cov_df = num_cov_df[,n_na_num==0]
+  n_uniq_num = sapply(num_cov_df, function(x) length(unique(x)))
+  num_cov_df = num_cov_df[,n_uniq_num>1]
+  
+  cat_cov_df[is.na(cat_cov_df)] = "UNKNOWN"
+  cat_cov_df[cat_cov_df=="99"] = "UNKNOWN"
+  cat_cov_df[cat_cov_df=="99.0"] = "UNKNOWN"
+  cat_cov_df[cat_cov_df=="98"] = "UNKNOWN"
+  cat_cov_df[cat_cov_df=="98.0"] = "UNKNOWN"
+  cat_cov_df[cat_cov_df=="97"] = "UNKNOWN"
+  cat_cov_df[cat_cov_df=="97.0"] = "UNKNOWN"
+  cat_cov_df[cat_cov_df=="UNK"] = "UNKNOWN"
+  
+  for(cov in categorical_covariates){
+    item_count = table(cat_cov_df[,cov])
+    items_to_avoid = names(item_count[item_count<min.samples])
+    cat_cov_df[cat_cov_df[,cov] %in% items_to_avoid, cov] = "UNKNOWN"
+    if(length(items_to_avoid)>0 && length(items_to_avoid) < 20)
+      print(paste(c(cov, ":", items_to_avoid), sep = " ", collapse = " " ))
+  }
+  
+  n_uniq_cat = sapply(cat_cov_df, function(x) length(unique(x)))
+  cat_cov_df = cat_cov_df[,n_uniq_cat>1]
+  
+  dat.cov = cbind(num_cov_df, cat_cov_df)
+  for(cov_name in colnames(num_cov_df))
+    dat.cov[,cov_name] = as.numeric(dat.cov[,cov_name])
+  for(cov_name in colnames(cat_cov_df))
+    dat.cov[,cov_name] = factor(dat.cov[,cov_name])
+  
+  ### call MARS model
   es.res <- multiEarthSelect(dat.expr, dat.cov, n.rep, n.cores, jackknife, group.within)
   print('after multiEarchSelect call')
   all.covs <- unique(do.call(c, lapply(es.res, rownames)))
@@ -237,7 +255,21 @@ main <- function(xp.file, cov.file, out.file, do.log, n.rep, n.cores, regress.co
 
 if ( ! interactive() ) {
   args <- getArgs()
-  main(args$expression, args$covariates, args$output, args$log, args$replicates, args$ncores, args$regress, args$jackknife, args$within)
+  
+  # args <- list(expression="/scratch1/battle-fs1/ashis/progdata/brain_process/v6/20170901.gtex_expression.brain.good_genes.outlier_rm.txt",
+  #              covariates="/scratch1/battle-fs1/ashis/progdata/brain_process/v6/covariates/20170901.all_covariates.PCs.brain.txt",
+  #              output="results/identified_brain_cov.txt",
+  #              log=1,
+  #              replicates=3,
+  #              ncores=5,
+  #              #regress="seq_pc1,seq_pc2,seq_pc3,seq_pc4,seq_pc5",
+  #              regress=NULL,
+  #              jackknife=0,
+  #              within="",
+  #              min_sample=15)
+  # 
+  
+  main(args$expression, args$covariates, args$output, args$log, args$replicates, args$ncores, args$regress, args$jackknife, args$within, args$min_sample)
 }
 
 
@@ -252,3 +284,4 @@ if ( ! interactive() ) {
 #    # expr is (n.g x n.s) so
 #    dat.expr <- as.matrix(dat.expr) %*% (diag(rep(1, ncol(dat.expr))) - mmat %*% solve(t(mmat) %*% mmat) %*% t(mmat))
 #  }
+
