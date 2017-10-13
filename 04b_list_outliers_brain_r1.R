e5adc8a9c2a9ff85a152845997f4579cf794a111
alorchhota <alorchhota@gmail.com>
2017-10-13 10:01:43 -0400

    outlier list updated

--- 04b_list_outliers_brain_r1.R
+++ 04b_list_outliers_brain_r1.R
@@ -19,63 +19,46 @@ covar.outliers <- covars$st_id[c(
 )]
 
 gene.pc.outliers <- rownames(gene.vals)[c(
-  which(gene.vals$BRNAMY.PC5 < -0.3),  # ??  "GTEX.15DDE.BRNAMY", extreme in both PC5 and PC6
-  which(gene.vals$BRNCTX.PC3 > 0.3),
-  which(gene.vals$BRNCTX.PC6 > 0.4),
   which(gene.vals$BRNCTXBA9.PC4 < -0.4),
-  which(gene.vals$BRNHIP.PC3 < -0.25),
   which(gene.vals$BRNHYP.PC5 < -0.4)
 )]
 
 isof.pc.outliers <- rownames(isof.vals)[c(
-  which(isof.vals$BRNCDT.PC5 < -0.4),
-  which(isof.vals$BRNCTXB24.PC5 < -0.3),  # ?? "GTEX.UTHO.BRNCTXB24" - sample indiv outlier in CDT and CTXBA9
+  which(isof.vals$BRNCDT.PC5 > 0.4),
   which(isof.vals$BRNCTXBA9.PC3 > 0.4),
   which(isof.vals$BRNHIP.PC6 < -0.35),
-  which(isof.vals$BRNHYP.PC6 < -0.4),
-  which(isof.vals$BRNPUT.PC6 < -0.4)
+  which(isof.vals$BRNHYP.PC6 > 0.5)
 )]
 
 isofpct.pc.outliers <- rownames(isofpct.vals)[c(
-  which(isofpct.vals$BRNACC.PC1 < -0.3),
+  which(isofpct.vals$BRNACC.PC1 > 0.3),
   which(isofpct.vals$BRNACC.PC4 > 0.6),
   which(isofpct.vals$BRNACC.PC5 > 0.4),
-  which(isofpct.vals$BRNACC.PC5 > 0.4),
-  which(isofpct.vals$BRNACC.PC6 > 0.35),  # ??  "GTEX.1E2YA.BRNACC" "GTEX.X4XY.BRNACC"(outlier in another tissue)
-  which(isofpct.vals$BRNAMY.PC4 > 0.4),   # ?? "GTEX.T6MN.BRNAMY" (outlier in BRNAMY PC5)  "GTEX.UTHO.BRNAMY" (outlier in other tissues)
-  which(isofpct.vals$BRNAMY.PC5 < -0.4),
-  which(isofpct.vals$BRNAMY.PC6 > 0.6),
+  which(isofpct.vals$BRNACC.PC6 < -0.35),
+  which(isofpct.vals$BRNAMY.PC4 > 0.6),
+  which(isofpct.vals$BRNAMY.PC6 > 0.4),
+  which(isofpct.vals$BRNAMY.PC6 < -0.6),
   
-  which(isofpct.vals$BRNCDT.PC4 > 0.6),
-  which(isofpct.vals$BRNCDT.PC5 > 0.4),
-  
-  which(isofpct.vals$BRNCTX.PC2 < -0.3),
-  which(isofpct.vals$BRNCTX.PC6 > 0.5),
-  
+  which(isofpct.vals$BRNCDT.PC4 < -0.6),
+
   which(isofpct.vals$BRNCTXB24.PC4 < -0.6),
   which(isofpct.vals$BRNCTXB24.PC6 > 0.4),
-  which(isofpct.vals$BRNCTXB24.PC6 < -0.4),
-  
+
   which(isofpct.vals$BRNCTXBA9.PC2 < -0.4),
-  which(isofpct.vals$BRNCTXBA9.PC3 > 0.3),
-  which(isofpct.vals$BRNCTXBA9.PC4 > 0.6),
-  which(isofpct.vals$BRNCTXBA9.PC6 > 0.6),
+  which(isofpct.vals$BRNCTXBA9.PC3 < -0.3),
+  which(isofpct.vals$BRNCTXBA9.PC4 < -0.6),
+  which(isofpct.vals$BRNCTXBA9.PC6 < -0.6),
   
-  which(isofpct.vals$BRNHIP.PC2 < -0.3),
-  which(isofpct.vals$BRNHIP.PC3 < -0.4),
   which(isofpct.vals$BRNHIP.PC4 > 0.6),
   which(isofpct.vals$BRNHIP.PC6 < -0.6),
   
-  which(isofpct.vals$BRNHYP.PC1 < -0.3),  # ?? "GTEX.14753.BRNHYP", outlier in PC5
-  which(isofpct.vals$BRNHYP.PC4 > 0.4),
-  which(isofpct.vals$BRNHYP.PC5 > 0.5),
+  which(isofpct.vals$BRNHYP.PC4 < -0.6),
+  which(isofpct.vals$BRNHYP.PC5 < -0.5),
   
   which(isofpct.vals$BRNPUT.PC2 < -0.4),
-  which(isofpct.vals$BRNPUT.PC4 < -0.5),
-  which(isofpct.vals$BRNPUT.PC5 > 0.4),
-  
-  which(isofpct.vals$BRNSNA.PC1 > 0.4),
-  which(isofpct.vals$BRNSNA.PC4 < -0.5),
+  which(isofpct.vals$BRNPUT.PC4 > 0.5),
+
+  which(isofpct.vals$BRNSNA.PC4 > 0.5),
   which(isofpct.vals$BRNSNA.PC5 < -0.5)
 )]
 
