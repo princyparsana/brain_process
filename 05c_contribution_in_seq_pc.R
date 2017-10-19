e323cf2f996de63720b9abbd3832d45c41fef43c
alorchhota <alorchhota@gmail.com>
2017-10-18 20:07:39 -0400

    contribution of cov in seq_pc

--- 05c_contribution_in_seq_pc.R
+++ 05c_contribution_in_seq_pc.R
@@ -1,5 +1,5 @@
-source("io_util.R")
 library(argparser)
+library(corrplot)
 
 ##### parse arguments ######
 args <- arg_parser("program");
@@ -28,6 +28,9 @@ seq_pc_df = cov_df[,SEQ_PCS]
 if(sum(is.na(seq_cov_df)) > 0)
   stop('NA values in sequence covariates!')
 
+n_uniq = sapply(seq_cov_df, function(x) length(unique(x)))
+seq_cov_df = seq_cov_df[, n_uniq>1]
+
 # compute cotribution of a covariate in each pc
 seq_cov_scaled_df = scale(seq_cov_df)
 cor_pc_cov = cor(seq_pc_df, seq_cov_scaled_df)
@@ -35,6 +38,7 @@ cov_contrib = apply(cor_pc_cov, 1, function(x) x^2/sum(x^2)*100)
 
 # plot
 pdf(plt_fn)
+corrplot(cor_pc_cov, is.corr=T, method = "circle", title = "cor(seq_pc, covariate)")
 par(las=1)
 par(omi=c(0,0.5,0,0))
 for(pcname in SEQ_PCS){
